8c40691003e10f9c719c86ad2c8580fa6a31b5f2
johan <jmarquezz@ulasalle.edu.pe>
2025-08-26 14:57:22 -0500

    practica 1

--- src/practica1/ejercicio1.kt
+++ src/practica1/ejercicio1.kt
@@ -1,2 +1,31 @@
-package practica1
 
+
+fun determinarNivelRendimiento(puntuacion: Int): String {
+    return when (puntuacion) {
+        in 0..3 -> "Inaceptable"
+        in 4..6 -> "Aceptable"
+        in 7..10 -> "Meritorio"
+        else -> "Puntuación inválida"
+    }
+}
+
+fun calcularDineroRecibido(salario: Double, puntuacion: Int): Double {
+    return if (puntuacion in 0..10) {
+        salario * (puntuacion / 10.0)
+    } else {
+        0.0
+    }
+}
+
+fun main() {
+    print("Puntuación del empleado (0-10): ")
+    val puntuacion = readLine()!!.toInt()
+    print("Salario mensual: ")
+    val salario = readLine()!!.toDouble()
+
+    val nivel = determinarNivelRendimiento(puntuacion)
+    val dinero = calcularDineroRecibido(salario, puntuacion)
+
+    println("Nivel de Rendimiento: $nivel")
+    println("Dinero Recibido: $$dinero")
+}
